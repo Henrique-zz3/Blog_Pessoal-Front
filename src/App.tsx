95b9250b1c4518dde6b2a25037071dc2d0652fa5
Henrique_zz3 <henriquetoto3001@gmail.com>
2024-05-08 09:15:49 -0300

    CRUD Categoria - Comentado

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
-import Cadastro from './paginas/cadastro/Cadastro';
-import Home from './paginas/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
+import Login from './paginas/login/Login'
+import Cadastro from './paginas/cadastro/Cadastro'
+import Home from './paginas/home/Home'
+import ListaTemas from './components/temas/listaTemas/ListaTemas'
+import FormularioTema from './components/temas/formularioTemas/FormularioTemas'
+import DeletarTema from './components/temas/deletarTemas/DeletarTemas'
 
 function App() {
-  return (
-    <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
-  );
+    return (
+        <AuthProvider>  {/* Compartilha com a Aplicação todos os dados do Contexto */}
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>  {/* Define o tamanho minimo das páginas */}
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />   {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser editado */}
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />     {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser excluído */}
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    )
 }
-export default App;
+
+export default App
